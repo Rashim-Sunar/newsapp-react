32bf63855303cffa4839cb1a84d2a16574c479ef
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-01-20 20:39:11 -0500

    Adding Previous and Next button to populate different pages of NewsItem

--- src/components/News.js
+++ src/components/News.js
@@ -6,7 +6,8 @@ export class News extends Component {
         super();
         this.state={            //setting state inside constructor
             articles : [],
-            loading : false
+            loading : false,
+            page : 1
         }
     }
 
@@ -15,7 +16,31 @@ export class News extends Component {
         let data = await fetch(url);
         let parseddata = await data.json();
         console.log(parseddata);
-        this.setState({articles : parseddata.articles});
+        this.setState({articles : parseddata.articles, totalResults : parseddata.totalResults});    
+        }
+
+        handlePreviousClick=async()=>{
+            let url=`https://newsapi.org/v2/everything?q=sports&from=2023-12-20&sortBy=publishedAt&apiKey=38756c3463f84422b9e5a053383311ac&page=${this.state.page-1}`;
+            let data = await fetch(url);
+            let parseddata = await data.json();
+            this.setState({
+                page : this.state.page-1,
+                articles : parseddata.articles
+            });
+        }
+
+        handleNextCLick=async()=>{
+            if(this.state.page + 1 >Math.ceil(this.state.totalResults/100)){
+                
+            }else{
+                let url=`https://newsapi.org/v2/everything?q=sports&from=2023-12-20&sortBy=publishedAt&apiKey=38756c3463f84422b9e5a053383311ac&page=${this.state.page + 1}`;
+                let data = await fetch(url);
+                let parseddata = await data.json();  
+                this.setState({
+                    page : this.state.page +1,
+                    articles : parseddata.articles
+                });
+            }
         }
 
   render() {
@@ -27,6 +52,10 @@ export class News extends Component {
                 <NewsItem title={element.title?element.title.slice(0,45):" "} description={element.description?element.description.slice(0,80):" "} imageUrl={element.urlToImage?element.urlToImage:"https://media.zenfs.com/en/ktla_articles_362/5ecb0820e4e59977a39d6d2fa0eb4a22"} newsUrl={element.url}/>
             </div>
             })}
+          </div>
+          <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.handlePreviousClick}><b>&larr;</b>Previous</button>
+                <button type="button" className="btn btn-primary" onClick={this.handleNextCLick}>Next<b>&rarr;</b></button>
           </div>
         </div>
 
